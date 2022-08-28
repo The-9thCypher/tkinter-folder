b14a1a468b673f127d28254cb22b00436d9cbbb4
Shyllon Olayinka <shyllonolayinka608@gmail.com>
2022-08-28 15:16:53 +0100

    import from json and csv

--- tkinter.py
+++ tkinter.py
@@ -109,150 +109,196 @@
 
 # root.mainloop()
 
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
-df= pd.DataFrame(np.random.randn(5,3), 
-index=['a','c','e','f','h'], 
-columns = ['one','two','three'])
+# df= pd.DataFrame(np.random.randn(5,3), 
+# index=['a','c','e','f','h'], 
+# columns = ['one','two','three'])
 
-df= df.reindex(['a','b','c','d','e','f','g','h'])
+# df= df.reindex(['a','b','c','d','e','f','g','h'])
 
-print(df)
+# print(df)
 
-print("replace NanN with 'o.' ")
+# print("replace NanN with 'o.' ")
 
-print(df['one'].fillna(0))
+# print(df['one'].fillna(0))
+
+
+# import pandas as pd 
+# import numpy as np 
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
+
+# df= pd.DataFrame(data,
+# index=['one','two','three','four','five'])
+
+
+# print(df)
+
+
+# import pandas as pd 
+# import numpy as np 
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
+
+# index=('one','two','three','four','five')
+
+# df= pd.DataFrame(data, index)
+
+
+# print(df)
+
+# import pandas as pd 
+# import numpy as np
+
+# import matplotlib.pyplot as plt 
+
+# Hours_worked= 4,2,5,4,
+
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
+
+# names=('ope','prince','gbemi','gbenga','bisi')
+
+# df= pd.DataFrame(data, names)
+
+# plt.plot(names,Hours_worked)
+# plt.show()
+
+# print(df)
+
+
+# import pandas as pd 
+# import numpy as np
+
+# import matplotlib.pyplot as plt 
+
+# Hours_worked= 4,2,5,4,3
+
+# salary= 6,7,12,3,10
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
+
+# names=('ope','prince','gbemi','gbenga','bisi')
+
+# df= pd.DataFrame(data, names)
+
+# plt.plot(names,Hours_worked)
+# plt.xlabel("names of workers")
+# plt.ylabel("Hours_worked")
+# plt.show()
+
+# print(df)
+
+
+# import pandas as pd 
+# import numpy as np
+
+# import matplotlib.pyplot as plt 
+
+# Hours_worked= 4,2,5,4,3
+
+# salary= 6,7,12,3,10
+
+# No_of_times_absent =1,4,2,3,5
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3], 'No of times absent': [1,4,2,3,5]}
+
+# names=('ope','prince','gbemi','gbenga','bisi')
+
+# df= pd.DataFrame(data, names)
+
+# plt.plot(names,salary)
+# plt.xlabel("names of workers")
+# plt.ylabel("salary(Thousand Naira)")
+# plt.show()
+
+# print(df)
+
+# import pandas as pd 
+# import numpy as np
+
+# import matplotlib.pyplot as plt 
+
+# Hours_worked= 4,2,5,4,3
+
+# salary= 6,15,7,3,10
+
+# No_of_times_absent =1,4,2,3,5
+
+# data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3], 'No of times absent': [1,4,2,3,5]}
+
+# names=('ope','prince','gbemi','gbenga','bisi')
+
+# df= pd.DataFrame(data, names)
+
+# plt.plot(names,salary,No_of_times_absent,Hours_worked)
+# plt.xlabel("names of workers")
+# plt.ylabel("salary(Thousand Naira)")
+# plt.show()
+
+# print(df)
+
+# import pandas as pd 
+# import numpy as np
+# import matplotlib.pyplot as plt 
+
+# data= {'name':['shyllon', 'dennis', 'ofe', 'fola'], 'class':['ss3a','ss3a','ss3a','ss3a']}
+
+# score=(95, 75,89,72)
+# index= ('1','2','3','4')
+
+# df= pd.DataFrame(data, index)
+
+# plt.plot(index, score)
+# plt.xlabel("students")
+# plt.ylabel("Exam scores")
+
+# print(df)
+
+
+# import pandas as pd 
+# import numpy as np 
+# df= pd.DataFrame(np.random.randn(5,3),
+# index=['a','c','e','f','h'], columns=['one','two','three'])
+
+# df=df.reindex(['a','b','c','d','e','f','g','h'])
+
+# print(df.fillna(method='pad'))
 
 
 import pandas as pd 
 import numpy as np 
+df= pd.DataFrame(np.random.randn(5,3),
+index=['a','c','e','f','h'], columns=['one','two','three'])
 
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
+df=df.reindex(['a','b','c','d','e','f','g','h'])
 
-df= pd.DataFrame(data,
-index=['one','two','three','four','five'])
-
-
-print(df)
-
-
-import pandas as pd 
-import numpy as np 
-
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
-
-index=('one','two','three','four','five')
-
-df= pd.DataFrame(data, index)
-
-
-print(df)
-
-import pandas as pd 
-import numpy as np
-
-import matplotlib.pyplot as plt 
-
-Hours_worked= 4,2,5,4,
-
-
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
-
-names=('ope','prince','gbemi','gbenga','bisi')
-
-df= pd.DataFrame(data, names)
-
-plt.plot(names,Hours_worked)
-plt.show()
-
-print(df)
-
-
-import pandas as pd 
-import numpy as np
-
-import matplotlib.pyplot as plt 
-
-Hours_worked= 4,2,5,4,3
-
-salary= 6,7,12,3,10
-
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3]}
-
-names=('ope','prince','gbemi','gbenga','bisi')
-
-df= pd.DataFrame(data, names)
-
-plt.plot(names,Hours_worked)
-plt.xlabel("names of workers")
-plt.ylabel("Hours_worked")
-plt.show()
-
-print(df)
-
-
-import pandas as pd 
-import numpy as np
-
-import matplotlib.pyplot as plt 
-
-Hours_worked= 4,2,5,4,3
-
-salary= 6,7,12,3,10
-
-No_of_times_absent =1,4,2,3,5
-
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3], 'No of times absent': [1,4,2,3,5]}
-
-names=('ope','prince','gbemi','gbenga','bisi')
-
-df= pd.DataFrame(data, names)
-
-plt.plot(names,salary)
-plt.xlabel("names of workers")
-plt.ylabel("salary(Thousand Naira)")
-plt.show()
-
-print(df)
-
-import pandas as pd 
-import numpy as np
-
-import matplotlib.pyplot as plt 
-
-Hours_worked= 4,2,5,4,3
-
-salary= 6,15,7,3,10
-
-No_of_times_absent =1,4,2,3,5
-
-data={'salary':[4600,129128,2412,31412,214], 'Hours Worked':[4,2,5,4,3], 'No of times absent': [1,4,2,3,5]}
-
-names=('ope','prince','gbemi','gbenga','bisi')
-
-df= pd.DataFrame(data, names)
-
-plt.plot(names,salary,No_of_times_absent,Hours_worked)
-plt.xlabel("names of workers")
-plt.ylabel("salary(Thousand Naira)")
-plt.show()
-
-print(df)
+print(df.dropna())
 
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt 
+df= pd.DataFrame(np.random.randn(5,3),
+index=['a','c','e','f','h'], columns=['one','two','three'])
 
-data= {'name':['shyllon', 'dennis', 'ofe', 'fola'], 'class':['ss3a','ss3a','ss3a','ss3a']}
+df=df.reindex(['a','b','c','d','e','f','g','h'])
 
-score=(95, 75,89,72)
-index= ('1','2','3','4')
+print(df.dropna())
 
-df= pd.DataFrame(data, index)
+plt.plot(df)
 
-plt.plot(index, score)
-plt.xlabel("students")
-plt.ylabel("Exam scores")
+import pandas as pd 
 
-print(df)
+data=  pd.read_csv('myfirstcsvfile.txt')
+
+data=pd.DataFrame(data)
+print(data)
+
+import pandas as pd 
+
+data=  pd.read_json('myfirstjsonfile.json')
+
+data=pd.DataFrame(data)
+print(data)
